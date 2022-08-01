062c717e166da6c9abcfcb9a0e01da7c9f913ff3
gemstoneyang <bennyws99@gmail.com>
2022-08-01 09:32:18 +0900

    s finish

--- 42cursus/ft_printf/ft_printf.h
+++ 42cursus/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: wonyang <wonyang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 16:25:12 by wonyang           #+#    #+#             */
-/*   Updated: 2022/07/31 18:47:06 by wonyang          ###   ########.fr       */
+/*   Updated: 2022/08/01 09:26:48 by wonyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,11 @@ t_info	*new_info(void);
 
 int		print_persent_blank(t_info *info);
 int		print_info_persent(t_info *info);
+
+int		ft_strlen_s(t_info *info, char *val_str);
+int		print_s_space(t_info *info, char *val_str);
+int		print_s_str(t_info *info, char *val_str);
+int		print_info_s(t_info *info, va_list ap);
 
 int		print_c_space(t_info *info);
 int		print_c_char(char val_char);
--- 42cursus/ft_printf/ft_printf_s.c
+++ 42cursus/ft_printf/ft_printf_s.c
@@ -6,18 +6,53 @@
 /*   By: wonyang <wonyang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 19:23:37 by wonyang           #+#    #+#             */
-/*   Updated: 2022/07/31 19:47:22 by wonyang          ###   ########.fr       */
+/*   Updated: 2022/08/01 09:30:57 by wonyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	ft_strlen_s(t_info *info, char *val_str)
+{
+	int	str_len;
+
+	str_len = ft_strlen(val_str);
+	if (info->period == TRUE && info->precision < str_len)
+		return (info->precision);
+	return (str_len);
+}
+
+int	print_s_space(t_info *info, char *val_str)
+{
+	int	i;
+	int	str_len;
+
+	i = 0;
+	if (val_str == NULL)
+		val_str = "(null)";
+	str_len = ft_strlen_s(info, val_str);
+	while (i < info->width - str_len)
+	{
+		write(1, " ", 1);
+		i++;
+	}
+	return (i);
+}
+
 int	print_s_str(t_info *info, char *val_str)
 {
 	int	i;
 
-	i = 0;
-	while (info->period == True && i < info->precision == 0)
+	if (val_str == NULL)
+		val_str = "(null)";
+	i = -1;
+	if (info->period == TRUE)
+		while (++i < info->precision && val_str[i])
+			write(1, val_str + i, 1);
+	else
+		while (val_str[++i])
+			write(1, val_str + i, 1);
+	return (i);
 }
 
 int	print_info_s(t_info *info, va_list ap)
@@ -26,7 +61,7 @@ int	print_info_s(t_info *info, va_list ap)
 	char	*val_str;
 
 	total_size = 0;
-	val_str= va_arg(ap, char *);
+	val_str = va_arg(ap, char *);
 	if (info->minus == TRUE)
 	{
 		total_size += print_s_str(info, val_str);
@@ -35,7 +70,7 @@ int	print_info_s(t_info *info, va_list ap)
 	else
 	{
 		total_size += print_s_space(info, val_str);
-		total_size += print_s_zero(info, val_str);
 		total_size += print_s_str(info, val_str);
 	}
+	return (total_size);
 }
