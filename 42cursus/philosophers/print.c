0d6049f65c4efcbc9ce5b171507f44ad3416b5d4
gemstoneyang <bennyws99@gmail.com>
2022-12-28 11:39:46 +0900

    print part mutex bug fix

--- 42cursus/philosophers/print.c
+++ 42cursus/philosophers/print.c
@@ -6,7 +6,7 @@
 /*   By: wonyang <wonyang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 11:08:03 by wonyang           #+#    #+#             */
-/*   Updated: 2022/12/28 11:15:23 by wonyang          ###   ########seoul.kr  */
+/*   Updated: 2022/12/28 11:38:42 by wonyang          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 void	print_log(t_philo *philo, char *msg)
 {
-	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	*print_mutex;
 
-	print_mutex = philo->info->print_mutex;
-	pthread_mutex_lock(&print_mutex);
+	print_mutex = &(philo->info->print_mutex);
+	pthread_mutex_lock(print_mutex);
 	pass_time(philo->info->start_time);
 	printf(" no.%d %s\n", philo->id, msg);
-	pthread_mutex_unlock(&print_mutex);
+	pthread_mutex_unlock(print_mutex);
 }
