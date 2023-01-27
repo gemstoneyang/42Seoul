463a5e0c2886ac96ec0cec2a49bae1470168db38
gemstoneyang <bennyws99@gmail.com>
2023-01-27 22:24:33 +0900

    [add] cpp00 ex02 init

--- 42cursus/CPP_Module/CPP00/test/main.cpp
+++ 42cursus/CPP_Module/CPP00/test/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 
+class Animal {
+  private:
+    int food;
+    int weight;
+
+  public:
+    Animal(int _food, int _weight) : food(_food), weight(_weight) {}
+    void increase_food(int inc);
+    void view_stat(void);
+};
+
+void Animal::increase_food(int inc) {
+  food += inc;
+  weight += (inc / 3);
+}
+void Animal::view_stat(void) {
+  std::cout << "food stat : " << food << std::endl;
+  std::cout << "weight stat : " << weight << std::endl;
+}
+
 int	main(void)
 {
-	char	*pBuffer = new char[128];
-	std::cout << "pBuffer 입력 : ";
-	std::cin.getline(pBuffer, sizeof(char) * 128);
-	std::cout << "result : " << pBuffer << std::endl;
-	delete[] pBuffer;
-	return 0;
+  Animal animal(10, 100);
+  animal.view_stat();
+  animal.increase_food(10);
+  animal.view_stat();
+  return 0;
 }
