00287708494c06e42d7e11c09c664a0be60ba718
gemstoneyang <bennyws99@gmail.com>
2023-02-02 22:38:56 +0900

    ex04

--- 42cursus/CPP_Module/CPP01/ex04/main.cpp
+++ 42cursus/CPP_Module/CPP01/ex04/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int	main(void) {
-	
+int	main(int argc, char **argv) {
+	if (argc != 4) {
+		std::cout << "invalid argument" << std::endl;
+		return 1;
+	}
+
+	std::string		filename = argv[1];
+	std::ifstream	instream(filename);
+
+	if (!instream.is_open()) {
+		std::cout << "invalid filename" << std::endl;
+		return 1;
+	}
+
+	std::string	s1 = argv[2];
+	std::string	s2 = argv[3];
+
+
 
 	return 0;
 }
