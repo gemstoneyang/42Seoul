b9473c0a5855fc2314509f9870e7d9659a56a7ea
gemstoneyang <bennyws99@gmail.com>
2023-02-09 13:42:12 +0900

    ex00

--- 42cursus/CPP_Module/CPP02/ex00/Fixed.hpp
+++ 42cursus/CPP_Module/CPP02/ex00/Fixed.hpp
@@ -9,17 +9,12 @@ class Fixed {
 public:
 	Fixed(void);
 	Fixed(const Fixed &fixed);
-	Fixed(const int num);
-	Fixed(const float num);
 	~Fixed(void);
 
 	Fixed&			operator=(const Fixed &fixed);
-	std::ostream&	operator<<(std::ostream& os, const Complex& c)
 
 	int		getRawBits(void) const;
 	void	setRawBits(int const raw);
-	float	toFloat(void) const;
-	int		toInt(void) const;
 
 private:
 	int					raw;
