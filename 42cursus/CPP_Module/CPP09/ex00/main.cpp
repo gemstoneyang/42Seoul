f99bf3456882fa9ad4b0c096ccecb4a370576758
gemstoneyang <bennyws99@gmail.com>
2023-08-09 18:10:13 +0900

    fix: add error message

--- 42cursus/CPP_Module/CPP09/ex00/main.cpp
+++ 42cursus/CPP_Module/CPP09/ex00/main.cpp
@@ -3,7 +3,10 @@
 #include "BitcoinExchange.hpp"
 
 int main(int argc, char **argv) {
-  if (argc != 2) return 0;
+  if (argc != 2) {
+    std::cout << "Error: Invalid argument" << std::endl;
+    return 0;
+  }
 
   try {
     BitcoinExchange::execute(argv[1]);
